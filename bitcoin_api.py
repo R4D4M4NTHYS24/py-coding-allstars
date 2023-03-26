f72fd36d500cb6e45b3f9f0de88879d4958ade0d
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-25 23:47:35 -0500

    test 2 for endpoint

--- bitcoin_api.py
+++ bitcoin_api.py
@@ -1,23 +1,8 @@
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from fastapi import FastAPI
 
-app = Flask(__name__)
-cors = CORS(app, origins=["http://localhost:4200"])
+app = FastAPI()
 
-@app.route('/api/prices')
-@cross_origin()
-def get_prices():
-    prices = {
-        'dolar': 4817.11,
-        'euro': 5165.68,
-        'yuan': 700.30
-    }
-
-    for key in prices:
-        prices[key] = round(prices[key], 2)
-
-    return jsonify(prices)
-
-
-if __name__ == '__main__':
-    app.run()
+@app.get("/bitcoin")
+async def get_bitcoin_price():
+    bitcoin_price = 60589.12 
+    return {"price": bitcoin_price}
